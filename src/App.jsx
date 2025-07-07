00f044837165caa393aca032c47f7fda1c2fba6f
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-07 21:35:06 +0200

    played with css template.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-const App = () => {
- 
-  return (
+
+
+
+export default function App() {
+
+  return(
     <>
-        <nav className="bg-indigo-700 border-b border-indigo-500">
+     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
           <div
@@ -202,6 +205,6 @@ const App = () => {
       >
     </section>
     </>
-  );
+  )
 };
-export default App;
+
