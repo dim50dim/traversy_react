58c563073c29e5394d3aee95c4c8c57c4c61dc5e
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-07 18:23:46 +0200

    touched basic things in react.

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,26 @@ const name = 'MAXXXXI'
 const x = 20;
 const y = 450;
 const names = ['Brad','Mary','Joy','Sara'];
+const loggeIn = true;
+const styles = {
+  color : 'blue',
+  fontSize: '55px'
+}
   return (
     <>
           <h1 className="text-3xl font-bold underline"> Hello everyone</h1>
           <p className='text-5xl'> Hello {name}</p>
+          <p style={styles}> css</p>
           <p>
             the sum of {x} and {y} is {x + y};
           </p>
-          {/* <ol>
-            {names.map((name,index) => {
-              <li key={index}>{name}</li>
-            })}
-          </ol> */}
+     
           <ul>
             {names.map((name,index) => {
               <li key={index}> {name} </li>
             })}
           </ul>
+          {! loggeIn ? <h1>Hello Member</h1> : <h2>Hello Guest</h2>  }
     </>
   )
 }
