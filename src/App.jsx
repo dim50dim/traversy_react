dc5335157e75e81825897594a780a13f6299e978
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-10 16:59:11 +0200

    removed last part of code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 
 
@@ -13,6 +14,7 @@ export default function App() {
   <Hero/>
   <HomeCards/>
   <JobListings/>
+  <ViewAllJobs/>
     {/* <!-- Hero --> */}
    
 
@@ -20,13 +22,7 @@ export default function App() {
 
 
  
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+
     </>
   )
 };
