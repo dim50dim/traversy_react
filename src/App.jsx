66cfac2910e20102f22af5efec9b58ffa654fc3c
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-11 19:44:22 +0200

    added routerProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
 import ViewAllJobs from "./components/ViewAllJobs";
-import{Route, createBrowserRouter,RouterProvider} from 'react-router-dom';
+import{Route, createBrowserRouter,RouterProvider, createRoutesFromElements} from 'react-router-dom';
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+)
 export default function App() {
 
   return(
-    <>
-  <Navbar/>
-  <Hero/>
-  <HomeCards/>
-  <JobListings/>
-  <ViewAllJobs/>
-    </>
+     <>
+        return <RouterProvider router ={router}/>
+     </>
   )
 };
 
