2333da59fb7a7d43d2e9c570faf96812cfcb35e2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-12 11:43:03 +0200

    well, now we rewrite all routes to App.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import JobListings from '..//components/JobListings'
 const HomePage = () => {
   return (
     <div>
       <Hero/>
+         <HomeCards/>
+         <JobListings/>
     </div>
   )
 }
