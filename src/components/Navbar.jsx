71aeedb5ade6cd44cfd2e2e1191de2e1e4e85e44
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-12 20:24:21 +0200

    a bit messy ,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             {/* <!-- Logo --> */}
-            <Link className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <div className="flex flex-shrink-0 items-center mr-4" href="/index.html">
               <img
                 className="h-10 w-auto"
                 src={logo}
@@ -20,7 +20,7 @@ function Navbar() {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span>
 
-            </Link>
+            
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <Link
