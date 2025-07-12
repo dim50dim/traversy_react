8d83302582933641fe6c4256ea899a4a068f6fee
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-12 11:36:55 +0200

    found it , it was mistake in Route component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
 import{Route, createBrowserRouter,RouterProvider, createRoutesFromElements} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MainLaoyt from './layots/MainLaoyt';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/about' index element={<HomePage/>} />)
+  createRoutesFromElements(
+  <Route path='/'  element={<MainLaoyt/>}>
+  <Route index element={<HomePage/>}/>
+  </Route>
+ )
 )
+
 export default function App() {
 
   return(
---
+++ src/layots/MainLaoyt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainLaoyt = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainLaoyt
