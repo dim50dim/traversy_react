1f777ed553d56aab075584dc0353eee14f889784
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-12 11:28:12 +0200

    still have this mistake on the main page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import Navbar from "./components/Navbar";
-import ViewAllJobs from "./components/ViewAllJobs";
+
 import{Route, createBrowserRouter,RouterProvider, createRoutesFromElements} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/about' index element={<h1>My App</h1>} />)
+  createRoutesFromElements(<Route path='/about' index element={<HomePage/>} />)
 )
 export default function App() {
 
