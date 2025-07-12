ea8608102a450ff443624c3adca1a3386f784f52
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-12 11:25:07 +0200

    so , have one specific mistake.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import ViewAllJobs from "./components/ViewAllJobs";
 import{Route, createBrowserRouter,RouterProvider, createRoutesFromElements} from 'react-router-dom';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1>My App</h1>} />)
+  createRoutesFromElements(<Route path='/about' index element={<h1>My App</h1>} />)
 )
 export default function App() {
 
   return(
      <>
-        return <RouterProvider router ={router}/>
+      <RouterProvider router ={router}/>
      </>
   )
 };
