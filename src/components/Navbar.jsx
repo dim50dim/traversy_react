47e7a09e393c92f07cbdb35db80ae2f848e74357
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-15 15:01:34 +0200

    added NavLink to head of Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from "../assets/images/logo.png"
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 function Navbar() {
    return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
