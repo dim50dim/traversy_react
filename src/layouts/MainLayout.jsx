c9394b14accb354c8d38a676e71459edef314ee6
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-15 14:53:08 +0200

    so, renew couple of lines of code and it works.

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar'
+const MainLayout = () => {
+  return (
+    <div>
+      <header>
+        <h1>My Job Site</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default MainLayout;
