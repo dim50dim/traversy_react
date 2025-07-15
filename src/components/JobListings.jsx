68757d655a975d13d5334c659834b58f506771c6
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-15 21:46:18 +0200

    added condition to JobListings .

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -6,7 +6,7 @@ const JobListings = ({isHome = false}) => {
     
   return (
     <div>
-         {/* <!-- Browse Jobs --> */}
+         {isHome ? 'Recent job' : 'Browse Jobs'}
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
