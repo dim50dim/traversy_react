b9f69caa04946561fa87bc965365c3a2d2056935
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-18 14:12:59 +0200

    so, now ,we play a little bit with css property flex.

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,24 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage'
-const router = createBrowserRouter(
-  createRoutesFromElements(
-   <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='jobs' element={<JobsPage />} />
-      <Route path='*' element={<NotFoundPage />} />
-      <Route path='/jobs/:id' element={<JobsPage />} />
+import Css from './pages/Css';
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//    <Route path='/' element={<MainLayout />}>
+//       <Route index element={<HomePage />} />
+//       <Route path='jobs' element={<JobsPage />} />
+//       <Route path='*' element={<NotFoundPage />} />
+//       <Route path='/jobs/:id' element={<JobsPage />} />
 
-    </Route>
- )
-)
+//     </Route>
+//  )
+// )
 
 export default function App() {
 
   return(
      <>
-      <RouterProvider router ={router}/>
+  <Css/>
      </>
   )
 };
---
+++ src/pages/Css.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Css = () => {
+  return (
+    <div className='container-1'>
+     <div className="box-1">
+        <h3>Box One</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet neque omnis deleniti.</p>
+     </div>
+     <div className="box-2">
+        <h3>Box Two</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet neque omnis deleniti.</p>
+     </div>
+     <div className="box-3">
+        <h3>Box Three</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis amet neque omnis delenit.</p>
+     </div>
+
+    </div>
+  )
+}
+
+export default Css
